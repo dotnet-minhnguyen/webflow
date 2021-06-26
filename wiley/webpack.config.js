ec9315c104cd3a34abee29c0ac66d26a34d9c65d
phuminh <phuminh@maysoft.vn>
2021-06-26 17:11:12 +0700

    build

--- wiley/webpack.config.js
+++ wiley/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   output: {
     filename  : 'index.js',
-    path      : path.resolve(__dirname, 'dist'),
+    path      : path.resolve(__dirname, 'lib'),
     publicPath: '/lib/'
   },
   target: 'node'
