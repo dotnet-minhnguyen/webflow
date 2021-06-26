d6f4bc47fcc7616b0cf5e6015b4cfaaeab7b1d9f
phuminh <phuminh@maysoft.vn>
2021-06-26 17:33:01 +0700

    dd

--- wiley/src/index.ts
+++ wiley/src/index.ts
@@ -1,8 +1,18 @@
-export default {
-    signUp: () => {
-      return "user"
-    },
-    login: () => {
-      return "login"
-    }
-  }
+//1. Map webflow cms collection to your models
+//2. Add some filed
+import $ from 'jquery'
+import _ from 'lodash'
+import htmlService from './services/html.service'
+import webflowService from './services/webflow.service'
+
+export const Wiley = {
+    countries: [],
+    events: [],
+    webinars: [],
+    products: [],
+    news: [],
+    testimonials: [],
+
+    webflowService,
+    htmlService
+}
